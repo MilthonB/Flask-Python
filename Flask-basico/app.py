c928eb98ac11830dcb76778b7e73f86b0bea3c23
Milthon <milthonbor@gmail.com>
2022-07-14 13:49:38 -0700

    markupsafe escape for evit DDOS

--- Flask-basico/app.py
+++ Flask-basico/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import flask
+from markupsafe import escape, Markup
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "<p> Hello Wrold!</p>"
+
+@app.route('/<name>')
+def hello(name):
+    print(name)
+    return f"Hello, {escape(name)}!"
